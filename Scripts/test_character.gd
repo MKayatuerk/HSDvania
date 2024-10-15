39409f0fb5d37ff9e9737891e3e42d74e4e3c1cc
Chefkoch Fern <meineMail@yahoo.com>
2024-10-15 12:34:03 +0200

    added moving platforms and a ladder, still WIP

--- Scripts/test_character.gd
+++ Scripts/test_character.gd
@@ -2,16 +2,37 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
+const CLIMBING_SPEED = -200
 const JUMP_VELOCITY = -400.0
 
 
+# Checks if the Doublejump upgrade has been collected
 var hasDoubleJumpUpgrade:bool = false
-var canDoubleJump = true
+
+# Checks if the character can currently doubleJump
+# Only effective when hasDoubleJumpUpgrade = true 
+var canDoubleJump:bool = true
+
+# Checks if the character can currently climb
+var canClimb:bool = false
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	print("can climb: " + str(canClimb) + " is on floor: " + str(is_on_floor())+ " can double Jump: " + str(canDoubleJump))
+	handleJump(delta)
+	handleMovement()
+	move_and_slide()
+
+
+
+
+# Function that handles the Jump functionality
+
+func handleJump(delta: float)-> void:
+	# Add the gravity.r
+	if  not (is_on_floor() and canClimb):
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -21,11 +42,13 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("jump") and !is_on_floor() and canCurrentlyDoubleJump():
 		canDoubleJump = !canDoubleJump
 		velocity.y = JUMP_VELOCITY - 150
-
+	#Enable double Jump when on the floor
 	if(velocity.y == 0):
 		canDoubleJump = true
 
 
+func handleMovement()-> void:
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -39,22 +62,43 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	climb()
 
 
-	move_and_slide()
+# climbing function based on the basic movement controller
+func climb() -> void:
+	var direction := Input.get_axis("down","up")
+	
+	if direction and canClimb:
+		velocity.y = direction * CLIMBING_SPEED
+	
 
-
+#Function that returns true if all conditions are met to allow the character
+#To double jump
 func canCurrentlyDoubleJump() -> bool:
 	if hasDoubleJumpUpgrade and canDoubleJump:
 		return true
 	else:
 		return false
-	
 
 
+
+
+##---------------------SIGNALS------------------------------------------------
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	hasDoubleJumpUpgrade = !hasDoubleJumpUpgrade
 
 
 func _on_enemy_got_stomped() -> void:
 	velocity.y = JUMP_VELOCITY
+
+
+func _on_ladder_body_entered(body: Node2D) -> void:
+	canClimb = true
+	
+
+
+func _on_ladder_body_exited(body: Node2D) -> void:
+	canClimb = false
+	
--- Scripts/testscene.gd
+++ Scripts/testscene.gd
@@ -4,3 +4,5 @@ extends Node2D
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+	elif Input.is_action_just_pressed("quit"):
+		get_tree().quit()
