ad68b2719a604336095c430bf3279810f0924712
Chefkoch Fern <meineMail@yahoo.com>
2024-11-03 20:17:25 +0100

    Added the Basic first floor geometry. Need to fix scene loading most likely through a scenemanager global

--- Scripts/awsome_creditsroom.gd
+++ Scripts/awsome_creditsroom.gd
@@ -1,11 +1,12 @@
-extends Area2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-
+func _ready() -> void:
+	print_tree_pretty()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("up") and has_overlapping_bodies():
-		get_tree().change_scene_to_file("res://Scenes/awsome_creditsroom.tscn")
+	pass
--- Scripts/testscene.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("reset"):
-		get_tree().reload_current_scene()
-	elif Input.is_action_just_pressed("quit"):
-		get_tree().quit()
